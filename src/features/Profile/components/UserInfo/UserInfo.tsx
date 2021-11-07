b3dc20c0856dd21cc6daf7fc36aab8bc8d80836a
Gabriel Oliveira <gabrielscorehero55@gmail.com>
2021-11-06 23:08:14 -0300

    feat: created follow section

--- src/features/Profile/components/UserInfo/UserInfo.tsx
+++ src/features/Profile/components/UserInfo/UserInfo.tsx
@@ -1,20 +1,35 @@
-import FadeIn from "react-fade-in";
-import { User } from "../../../../core/models";
-import { VscGithubInverted } from "../../../components/Icons";
+import { useEffect, useState } from "react";
 import style from "./info.module.scss";
+
+import { VscGithubInverted } from "../../../components/Icons";
+import { User } from "../../../../core/models";
+import FadeIn from "react-fade-in";
+import { api } from "../../../../core/services/api";
 
 interface UserInfoProps {
   user?: User;
 }
 
 function UserInfo({ user }: UserInfoProps) {
-  const backgroundBanner = `https://source.unsplash.com/random?sig=${Math.random()}`;
+  const [follows, setFollows] = useState<User[] | undefined>(undefined);
+
+  useEffect(() => {
+    async function loadFollows() {
+      const response = await api.get<User[]>("/follows");
+      setFollows(response.data);
+    }
+
+    loadFollows();
+  }, []);
+
+  const randomBanner = () =>
+    `https://source.unsplash.com/random?sig=${Math.random()}`;
 
   return (
     <section className={style.userInfoWrapper}>
       <div className={style.userInfoContent}>
         <div className={style.userInfoBanner}>
-          <img src={backgroundBanner} alt="Background Banner" />
+          <img src={randomBanner()} alt="Background Banner" />
         </div>
 
         <div className={style.informationWrapper}>
@@ -47,7 +62,33 @@ function UserInfo({ user }: UserInfoProps) {
         </div>
       </div>
 
-      <div className={style.userInfoFollows}>Other Side</div>
+      <div className={style.followsWrapper}>
+        <h1>Seguidores</h1>
+
+        <ul className={style.followsList}>
+          {follows ? (
+            follows.map((user) => {
+              return (
+                <li key={user.id}>
+                  <div className={style.followAvatar}>
+                    <img src={user.avatar_url} alt={user.name} />
+                  </div>
+
+                  <div className={style.followUser}>
+                    <h4>{user?.name}</h4>
+                    <div className={style.followRepoUser}>
+                      <VscGithubInverted size={16} />
+                      {user?.login}
+                    </div>
+                  </div>
+                </li>
+              );
+            })
+          ) : (
+            <li>Você não tem seguidores</li>
+          )}
+        </ul>
+      </div>
     </section>
   );
 }
