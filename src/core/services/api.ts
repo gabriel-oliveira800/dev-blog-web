bd2e114f0f836fbe3db099a567d5e9ba5467aede
Gabriel Oliveira <gabrielscorehero55@gmail.com>
2021-11-27 23:58:26 -0300

    fix: add logout in status code 412

--- src/core/services/api.ts
+++ src/core/services/api.ts
@@ -1,7 +1,24 @@
+import { useHistory } from "react-router-dom";
 import axios from "axios";
+
+import { ApplicationStore } from "./applicationStore";
+import { AppRoutes } from "../values";
 
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_API_URL}`,
 });
+
+api.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    if (error.message.includes("412")) {
+      ApplicationStore.handleLogout();
+      useHistory().push(AppRoutes.login);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export { api };
